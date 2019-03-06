34d5980a4b2a7cee2748a0f9ef631261492359c6
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2019-03-06 09:08:58 +0100

    Minor cleanups and add .editorconfig

--- src/Blockchain.v
+++ src/Blockchain.v
@@ -1,4 +1,5 @@
 From Coq Require Import String.
+
 From Coq Require OrderedTypeEx.
 From SmartContracts Require Import Oak.
 From SmartContracts Require Import Monads.
@@ -30,7 +31,7 @@ Inductive TxBody :=
   | tx_empty
   | tx_deploy (deployment : ContractDeployment)
   | tx_call (message : OakValue).
-      
+
 Record Tx :=
   build_tx {
     tx_from : Address;
@@ -72,45 +73,47 @@ Instance impl_interface : interface :=
    todo: come back to this and understand universe polymorphism in depth. *)
 Polymorphic Record ChainInterface :=
   build_chain_interface {
-    ifc_chain_type : Type;
-    ifc_chain_at : ifc_chain_type -> BlockId -> option ifc_chain_type;
-    ifc_head_block : ifc_chain_type -> Block;
-    ifc_incoming_txs : ifc_chain_type -> Address -> list Tx;
-    ifc_outgoing_txs :  ifc_chain_type -> Address -> list Tx;
-    ifc_contract_deployment :  ifc_chain_type -> Address -> option ContractDeployment;
-    ifc_contract_state : ifc_chain_type -> Address -> option OakValue;
+    (* Would be nice to encapsulate ChainInterface somewhat here
+       and avoid these ugly prefixed names *)
+    ci_chain_type : Type;
+    ci_chain_at : ci_chain_type -> BlockId -> option ci_chain_type;
+    ci_head_block : ci_chain_type -> Block;
+    ci_incoming_txs : ci_chain_type -> Address -> list Tx;
+    ci_outgoing_txs :  ci_chain_type -> Address -> list Tx;
+    ci_contract_deployment :  ci_chain_type -> Address -> option ContractDeployment;
+    ci_contract_state : ci_chain_type -> Address -> option OakValue;
   }.
 
 Record Chain :=
   build_chain {
-    chain_ifc : ChainInterface;
-    chain_val : ifc_chain_type chain_ifc
+    chain_ci : ChainInterface;
+    chain_val : chain_ci.(ci_chain_type);
   }.
 
 Definition chain_at (c : Chain) (bid : BlockId) : option Chain :=
-  do x <- c.(chain_ifc).(ifc_chain_at) c.(chain_val) bid;
-     Some {| chain_val := x |}.
+  do x <- c.(chain_ci).(ci_chain_at) c.(chain_val) bid;
+    Some {| chain_val := x |}.
 
 Definition head_block (c : Chain) :=
-  c.(chain_ifc).(ifc_head_block) c.(chain_val).
+  c.(chain_ci).(ci_head_block) c.(chain_val).
 
 Definition block_at (c : Chain) (bid : BlockId) : option Block :=
   do c <- chain_at c bid; Some (head_block c).
 
 Definition incoming_txs (c : Chain) :=
-  c.(chain_ifc).(ifc_incoming_txs) c.(chain_val).
+  c.(chain_ci).(ci_incoming_txs) c.(chain_val).
 
 Definition outgoing_txs (c : Chain) :=
-  c.(chain_ifc).(ifc_outgoing_txs) c.(chain_val).
+  c.(chain_ci).(ci_outgoing_txs) c.(chain_val).
 
 Definition contract_deployment (c : Chain) :=
-  c.(chain_ifc).(ifc_contract_deployment) c.(chain_val).
+  c.(chain_ci).(ci_contract_deployment) c.(chain_val).
 
 Definition contract_state (c : Chain) :=
-  c.(chain_ifc).(ifc_contract_state) c.(chain_val).
+  c.(chain_ci).(ci_contract_state) c.(chain_val).
 
 Inductive ContractCallContext :=
-build_contract_call_ctx {
+  build_contract_call_ctx {
     (* Chain *)
     ctx_chain : Chain;
     (* Address sending the funds *)
@@ -119,17 +122,17 @@ build_contract_call_ctx {
     ctx_contract_address : Address;
     (* Amount of GTU passed in call *)
     ctx_amount : Amount;
-}.
+  }.
 
 Inductive ChainAction :=
-| act_transfer (to : Address) (amount : Amount)
-| act_call (to : Address) (amount : Amount) (msg : OakValue)
-| act_deploy
-    {setup_ty msg_ty state_ty : Type}
-    (version : Version)
-    (init : ContractCallContext -> setup_ty -> option state_ty)
-    (receive : ContractCallContext -> state_ty ->
-                option msg_ty -> option (state_ty * list ChainAction)).
+  | act_transfer (to : Address) (amount : Amount)
+  | act_call (to : Address) (amount : Amount) (msg : OakValue)
+  | act_deploy
+      {setup_ty msg_ty state_ty : Type}
+      (version : Version)
+      (init : ContractCallContext -> setup_ty -> option state_ty)
+      (receive : ContractCallContext -> state_ty ->
+                 option msg_ty -> option (state_ty * list ChainAction)).
 
 (*
 Record ContractInterface (setup_ty msg_ty state_ty : Type) :=
