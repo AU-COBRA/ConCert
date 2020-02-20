5fe006bbfaa2ec29d0424ad3285a2b89df12549a
Milo <mikkelmilo@gmail.com>
2020-02-20 13:59:51 +0100

    more generators

--- execution/theories/Generators.v
+++ execution/theories/Generators.v
@@ -12,24 +12,24 @@ Export MonadNotation. Open Scope monad_scope.
 
 From Coq Require Import List.
 From Coq Require Import Strings.BinaryString.
-Import ListNotations.
+From Coq Require Import Morphisms.
 
-Record BaseGens (BaseTypes : ChainBase) := 
+Import ListNotations.
+Close Scope address_scope.
+
+Record ChainContext (BaseTypes : ChainBase) := 
   mkBaseGens {
-    (* Address          : Type; *)
     gAddress              : G (@Address BaseTypes);
-    init_account_balances : @Address BaseTypes -> Amount;
+    accounts              : list (@Address BaseTypes);
+    contracts             : list (@Address BaseTypes);
     gValidContractAddr    : G (@Address BaseTypes);
-    (* genSetup         : G Setup;
-    genMsg           : G Msg;
-    genState         : G State;
-    genSerializedVal : G SerializedValue; *)
+    gInvalidContractAddr  : G (@Address BaseTypes);    
   }.
   
 Open Scope string_scope.
-Instance showBaseGens {BaseTypes : ChainBase} : Show (BaseGens BaseTypes)  :=
+Instance showBaseGens {BaseTypes : ChainBase} : Show (ChainContext BaseTypes)  :=
   {|
-    show bg := "BaseGens{...}"
+    show bg := "ChainContext{...}"
   |}.
 
 
@@ -62,7 +62,7 @@ Instance showEnvironment (BaseTypes : ChainBase) `{Show Chain}: Show Environment
 Close Scope string_scope.
 Open Scope list_scope.
 
-Fixpoint mkMapFromList {A B : Type}
+Fixpoint mkMapFromLists {A B : Type}
                        (a_eqb : A -> A -> bool)
                        (default : B)
                        (l : list A) 
@@ -71,24 +71,34 @@ Fixpoint mkMapFromList {A B : Type}
   match (l,lb) with
   | ([],[]) => fun x => default
   | (a::l', b::lb') => 
-    fun (x : A) => if a_eqb x a then b else (mkMapFromList a_eqb default l' lb') x 
+    fun (x : A) => if a_eqb x a then b else (mkMapFromLists a_eqb default l' lb') x 
   | (_,_) => fun x => default
   end.
 
+(* returns a generator in the given interval (inclusive).
+   If low > high, then it will always generate the value high *)
+Definition gIntervalNat (low : nat) (high : nat) : G nat :=
+  offset <- arbitrarySized (high - low) ;; returnGen (low + offset).
+
 (* Makes a generator for BlockChains, given generators for the necessary types: Address, Setup, Msg, State, SerializedValue. 
    Ensures that all address usages are consistent with the generated address mapping. *)
-Definition mkChainGen (BaseTypes : ChainBase) (baseGens : BaseGens BaseTypes) (n : nat)
+Definition mkChainGen (BaseTypes : ChainBase) (ctx : ChainContext BaseTypes) (n : nat)
                       : G Chain  := 
-    ch_height  <- arbitrarySized n ;;
-    cur_slot   <- arbitrarySized ch_height ;; (* We make sure current slot is <= chain height *)
-    let acc_bal := init_account_balances BaseTypes baseGens in
-    returnGen (build_chain ch_height cur_slot n acc_bal).
+    fin_height  <- arbitrarySized n ;;
+    cur_slot   <- arbitrarySized fin_height ;; (* We make sure current slot is <= finalized height *)
+    amounts <- vectorOf (length (@accounts BaseTypes ctx)) arbitrary ;;
+    let acc_bal := mkMapFromLists address_eqb 0%Z (accounts _ ctx) amounts in
+    returnGen (build_chain n cur_slot fin_height acc_bal). 
     
 (* For now, shrinking does nothing *)
 Instance shrinkChain (BaseTypes : ChainBase) : Shrink (@Chain BaseTypes) :=
   {|
     shrink c := cons c nil
   |}.
+
+Definition mkChainStateGen (env : Environment)
+                           (actionList : list Action)
+                           : G ChainState := returnGen (build_chain_state env actionList).
 
 
 Derive Arbitrary for SerializedType.
@@ -145,8 +155,112 @@ Instance genSerializedValueSized : GenSized SerializedValue :=
     arbitrarySized := gSerializedValueSized 
   |}.
 
+  
 (* Sample (@arbitrarySized SerializedType _ 3). *)
 (* Sample (@arbitrarySized SerializedValue _ 3). *)
+
+(* The Contract, WeakContract, and ContractCallContext types *)
+
+Definition gContractCallContext {BaseTypes : ChainBase} 
+                                (ctx : ChainContext BaseTypes) 
+                                : G ContractCallContext :=
+  liftM3 build_ctx (gAddress _ ctx) (gValidContractAddr _ ctx) arbitrary.
+  (* TODO: what kind of address is the first argument? should it be a contract address, or a non-contract address?
+     also, maybe replace the '_' with 'BaseTypes' if we get bugs *)
+
+
+Definition mkContractGen (Setup Msg State : Type)
+                        `{Serializable Setup}
+                        `{Serializable Msg}
+                        `{Serializable State}
+                        (init :
+                          Chain ->
+                          ContractCallContext ->
+                          Setup ->
+                          option State)
+                        (init_proper :
+                          Proper (ChainEquiv ==> eq ==> eq ==> eq) init)
+                        (receive :
+                          Chain ->
+                          ContractCallContext ->
+                          State ->
+                          option Msg ->
+                          option (State * list ActionBody))
+                        (receive_proper :
+                          Proper (ChainEquiv ==> eq ==> eq ==> eq ==> eq) receive)
+                        : G (Contract Setup Msg State) := returnGen (build_contract init init_proper receive receive_proper).
+
+Definition gWeakContractFromContract {Setup Msg State : Type}
+                                    `{Serializable Setup}
+                                    `{Serializable Msg}
+                                    `{Serializable State}
+                                     : (Contract Setup Msg State) -> WeakContract 
+                                     := contract_to_weak_contract.
+
+
+Definition gContractInterfaceFromSendAction {Msg : Type} 
+                                            {BaseTypes : ChainBase} 
+                                            (ctx : ChainContext BaseTypes)
+                                            (send : Amount -> option Msg -> ActionBody) 
+                                            : G (ContractInterface Msg) := 
+  addr <- (gValidContractAddr _ ctx) ;;
+  returnGen (build_contract_interface Msg addr send).
+
+Definition gDeploymentAction
+  {Setup Msg State : Type}
+ `{Serializable Setup}
+ `{Serializable Msg}
+ `{Serializable State}
+  {BaseTypes : ChainBase} 
+  (contract : @Contract BaseTypes Setup Msg State _ _ _)
+  (setup : Setup) : G ActionBody :=
+  amount <- arbitrary ;;
+  returnGen (act_deploy amount contract (serialize setup)).
+
+Definition gTransferAction {BaseTypes : ChainBase} 
+                           (ctx : ChainContext BaseTypes) 
+                           : G ActionBody 
+                           := liftM2 act_transfer (gInvalidContractAddr _ ctx) arbitrary.
+
+Definition gCallAction {Msg : Type}
+                      `{Serializable Msg}
+                       {BaseTypes : ChainBase} 
+                       (ctx : ChainContext BaseTypes)
+                       (gMsg : G Msg) 
+                       : G ActionBody 
+                       := liftM3 act_call (gInvalidContractAddr _ ctx) arbitrary (liftM serialize gMsg).
+
+Definition gActionBodyFromContract {Setup Msg State : Type}
+                                  `{Serializable Setup}
+                                  `{Serializable Msg}
+                                  `{Serializable State}
+                                   {BaseTypes : ChainBase} 
+                                   (ctx : ChainContext BaseTypes)
+                                   (gSetup : G Setup)
+                                   (gMsg : G Msg)
+                                   (c : @Contract BaseTypes Setup Msg State _ _ _) 
+                                   : G ActionBody 
+  := freq [
+    (1, gCallAction ctx gMsg);
+    (1, setup <- gSetup ;; (gDeploymentAction c setup));
+    (1, gTransferAction ctx)
+  ].
+
+Definition gActionFromContract  {Setup Msg State : Type}
+                               `{Serializable Setup}
+                               `{Serializable Msg}
+                               `{Serializable State}
+                                {BaseTypes : ChainBase} 
+                                (ctx : ChainContext BaseTypes)
+                                (gSetup : G Setup)
+                                (gMsg : G Msg)
+                                (c : @Contract BaseTypes Setup Msg State _ _ _) 
+                                : G Action := 
+                                actionbody <- gActionBodyFromContract ctx gSetup gMsg c ;;
+                                addr <- (@gInvalidContractAddr BaseTypes ctx) ;;
+                                returnGen (build_act addr actionbody).
+                                (* TODO: what kind of address should we be generating here? *)
+
 
 Import BoundedN.Stdpp.
 Import LocalBlockchain.
@@ -222,8 +336,12 @@ Instance genAddress : Gen (@Address LocalChainBase) :=
     arbitrary := @arbitrary (BoundedN.BoundedN AddrSize) genBoundedN 
   |}.
 
+(* Sample (@arbitrary (@Address LocalChainBase) _). *)
+
 (* Definition genDummyChainedList : G (ChainTrace empty_state (build_chain_state lcb_lc [])) :=
   returnGen clnil. *)
+
+
 
 Definition gDummyLocalChain : G LocalChain :=
   returnGen lc_initial.
@@ -248,22 +366,35 @@ Definition gValidContractAddr' : G (@Address LocalChainBase) :=
   | None => returnGen (BoundedN.of_Z_const AddrSize (Z.of_N ContractAddrBase)) 
   end.
 
+(* a generator which makes values n such that 0 <= n < ContractAddrBase*)
+Definition gInvalidValidContractAddr' : G (@Address LocalChainBase) :=
+  n <- arbitrarySized ((N.to_nat ContractAddrBase) - 1) ;; 
+  let bound_opt : option (BoundedN.BoundedN AddrSize):= @decode_bounded AddrSize (Pos.of_nat n) in
+  match bound_opt with
+  | Some b => returnGen b
+  | None => returnGen (BoundedN.of_Z_const AddrSize (Z.of_N 0)) 
+  end.
 
-Definition gLocalBaseGens (n : nat) : G (BaseGens LocalChainBase) := 
+
+Definition gLocalChainContext (n : nat) : G (ChainContext LocalChainBase) := 
   let addr_eqb := @address_eqb LocalChainBase in 
   let default : Amount := Z0 in
+  let gAddr := (@arbitrary (@Address LocalChainBase) _) in
   amounts    <- vectorOf n arbitrary ;;
-  addrs      <- vectorOf n arbitrary ;;
-  let acc_bal := mkMapFromList addr_eqb default addrs amounts in
-  returnGen (mkBaseGens LocalChainBase arbitrary acc_bal gValidContractAddr').
+  contracts      <- vectorOf n gValidContractAddr' ;;
+  accounts      <- vectorOf n gInvalidValidContractAddr' ;;
+  let contractBaseAddr := BoundedN.of_Z_const AddrSize (Z.of_N ContractAddrBase) in
+  let gContractAddr := elems_ contractBaseAddr contracts in
+  let gAccountAddr := elems_ zero_address accounts in
+  returnGen (mkBaseGens LocalChainBase gAddr accounts contracts gValidContractAddr' gInvalidValidContractAddr').
 
-Instance genLocalBaseGens : GenSized (BaseGens LocalChainBase) :=
+Instance genLocalBaseGens : GenSized (ChainContext LocalChainBase) :=
   {|
-    arbitrarySized := gLocalBaseGens
+    arbitrarySized := gLocalChainContext
   |}.
 
 Definition gLocalChainSized : nat -> G (@Chain LocalChainBase) := 
-  fun n => lb <- gLocalBaseGens n ;; mkChainGen LocalChainBase lb n.
+  fun n => lb <- gLocalChainContext n ;; mkChainGen LocalChainBase lb n.
 
   
 Instance genLocalChainSized : GenSized (@Chain LocalChainBase) :=
@@ -280,13 +411,14 @@ Close Scope N_scope.
 
 
 Open Scope list_scope.
-Definition acc_bal := mkMapFromList (fun x y => x =? y) 42 [10;20;30;40] [1;2;3;4].
+Definition acc_bal := mkMapFromLists (fun x y => x =? y) 42 [10;20;30;40] [1;2;3;4].
 Definition testGChain : G Chain := arbitrary.
 
 
 Sample (gLocalChainSized 4).
 (* Sample (@arbitrarySized Chain _ 2). *)
-Sample (gLocalBaseGens 10).
+Sample (gLocalChainContext 10).
+Sample (bh <- gLocalChainContext 2 ;; @gAddress LocalChainBase bh). (* IMPORTANT NOTE: if we omit the explicit types, it will not work *)
 Sample (gEnvFromLocalChain lc_initial).
 
 Open Scope string_scope.
@@ -304,25 +436,48 @@ Instance showBlockHeader (BaseTypes : ChainBase) `{Show (@Address BaseTypes)} : 
 
 Close Scope string_scope.
 
+(* make a generator for a BlockHeader from a given chain.
+   Should satisfy the IsValidNextBlock predicate. *)
+Definition mkBlockHeaderGenSized (BaseTypes : ChainBase) 
+                                 (ctx : ChainContext BaseTypes) 
+                                 (c : @Chain BaseTypes) 
+                                 (n : nat)
+                                 : G (@BlockHeader BaseTypes) :=
+  let gAddr : G (@Address BaseTypes) := @gInvalidContractAddr BaseTypes ctx in
+  slot_offset    <- arbitrarySized n ;;
+  reward  <- liftM Z.of_nat arbitrary  ;;
+  creator <- gAddr ;;
+  let height := 1 + chain_height c in
+  let slot := slot_offset + current_slot c + 1 in
+  fin_height <- gIntervalNat (finalized_height c) (height - 1) ;; (*fin_height c <= fin_height < block_height*)
+  returnGen (@build_block_Header BaseTypes height slot fin_height reward creator).
 
-Definition mkBlockHeaderGenSized (BaseTypes : ChainBase) (baseGens : BaseGens BaseTypes) (n : nat)
-                            : G (@BlockHeader BaseTypes) :=
-  height  <- arbitrarySized n ;;
-  slot    <- arbitrarySized height ;;
-  reward  <- arbitrary ;;
-  creator <- gAddress BaseTypes baseGens ;;
-  returnGen (build_block_Header height slot n reward creator).
+Definition gLocalBCBlockHeaderSizedFromChain : nat -> (@Chain LocalChainBase) -> G (@BlockHeader LocalChainBase) := 
+  fun n c => ctx <- gLocalChainContext n ;; mkBlockHeaderGenSized LocalChainBase ctx c n.
 
 Definition gLocalBCBlockHeaderSized : nat -> G (@BlockHeader LocalChainBase) := 
-  fun n => baseGens <- gLocalBaseGens n ;; mkBlockHeaderGenSized LocalChainBase baseGens n.
+  fun n => c <- arbitrarySized n ;; gLocalBCBlockHeaderSizedFromChain n c.
 
 Instance genLocalBCBlockHeaderSized : GenSized (@BlockHeader LocalChainBase) :=
   {|
     arbitrarySized := gLocalBCBlockHeaderSized
   |}.
 
-Sample (@arbitrary (@BlockHeader LocalChainBase) _).
+Definition blockHeader_ex : (@BlockHeader LocalChainBase) := build_block_Header 0 0 0 0 zero_address.
+Definition gbh_dummy := returnGen blockHeader_ex.
 
-Definition validate_header_P := forAll (arbitrarySized 2) (fun header => match validate_header header lc_initial with Some _ => true | None => false end).
+Compute (show blockHeader_ex).
+Sample gbh_dummy.
 
-QuickChick validate_header_P.
+Sample (gLocalBCBlockHeaderSized 2).
+
+Open Scope core_scope.
+Definition validate_header_P : BlockHeader * Chain -> bool :=  fun p => match validate_header (fst p) (snd p) with Some _ => true | None => false end.
+
+(* QuickChick (forAll 
+  (c <- arbitrary ;; 
+  n <- arbitrary ;;
+  header <- gLocalBCBlockHeaderSizedFromChain n c ;;
+  returnGen (header, c)) 
+  validate_header_P). *)
+(* coqtop-stdout:+++ Passed 10000 tests (0 discards) *)
