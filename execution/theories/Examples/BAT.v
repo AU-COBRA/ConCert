553043ea4e0298b2a8ae023d5169f470fd3ee9d6
Mikkel Milo <mikkelmilo@gmail.com>
2020-03-22 21:01:48 +0100

    first draft of BAT implementation done

--- execution/theories/Examples/BAT.v
+++ execution/theories/Examples/BAT.v
@@ -1,5 +1,6 @@
-(* Implementation of the Basic Attention Token. 
-   Ported from https://github.com/brave-intl/basic-attention-token-crowdsale/blob/66c886cc4bfb0493d9e7980f392ca7921ef1e7fc/contracts/BAToken.sol
+(* 
+	Implementation of the Basic Attention Token. 
+  Ported from https://github.com/brave-intl/basic-attention-token-crowdsale/blob/66c886cc4bfb0493d9e7980f392ca7921ef1e7fc/contracts/BAToken.sol
 *)
 From Coq Require Import ZArith.
 From Coq Require Import Morphisms.
@@ -25,10 +26,8 @@ Definition TokenValue := EIP20Token.TokenValue.
 Open Scope N_scope.
 
 Inductive Msg := 
-  (* Message types from the EIP20/ERC20 Standard Token: *)
-	| transfer : Address -> TokenValue -> Msg
-	| transfer_from : Address -> Address -> TokenValue -> Msg
-	| approve : Address -> TokenValue -> Msg
+	(* Message types from the EIP20/ERC20 Standard Token: *)
+	| tokenMsg : EIP20Token.Msg -> Msg
   (* Message types specific for BAT: *)
   (* create_tokens acceps the currency of the chain and converts it to BAT according to the pre-specified exchange rate *)
   | create_tokens : Msg
@@ -37,13 +36,10 @@ Inductive Msg :=
   (* Allows contributors to recover their ether in the case of a failed funding campaign. *)
   | refund : Msg.
 
-
 Record State :=
 	build_state {
 		(* State from EIP20Token: *)
-		total_supply : N;
-		balances     : FMap Address TokenValue;
-		allowances   : FMap Address (FMap Address TokenValue);
+		token_state       : EIP20Token.State;
 		(* State for BAT: *)
 		fundDeposit    		: Address;
 		batFundDeposit 		: Address;
@@ -69,7 +65,7 @@ Record Setup :=
 	}.
 
 Instance state_settable : Settable _ :=
-	settable! build_state <total_supply; balances; allowances; fundDeposit; batFundDeposit; 
+	settable! build_state <token_state; fundDeposit; batFundDeposit; 
 	isFinalized; fundingStart; fundingEnd; tokenExchangeRate; tokenCreationCap; tokenCreationMin>.
 Instance setup_settable : Settable _ :=
   settable! build_setup <_batFund; _fundDeposit; _batFundDeposit; _fundingStart; _fundingEnd; _tokenExchangeRate; _tokenCreationCap; _tokenCreationMin>.
@@ -80,28 +76,24 @@ Global Instance setup_serializable : Serializable Setup :=
   Derive Serializable Setup_rect <build_setup>.
 
 Global Instance msg_serializable : Serializable Msg :=
-  Derive Serializable Msg_rect <transfer, transfer_from, approve, create_tokens, finalize, refund>.
+  Derive Serializable Msg_rect <tokenMsg, create_tokens, finalize, refund>.
 
 Global Instance state_serializable : Serializable State :=
 	Derive Serializable State_rect <build_state>.
 	
 End Serialization.
 
-Section BATtoEIP20Token.
-(* here we make sure that the functionality related to transfer, transfer_from, and approve uses the logic from the EIP20Token contract *)
-(* So essentially we're doing 'extension' as understood in mainstream object-oriented languages. *)
-
-
-End BATtoEIP20Token.
-
 Definition init (chain : Chain)
 								(ctx : ContractCallContext)
 								(setup : Setup) : option State := 
+	let token_state := {|
+			EIP20Token.balances := FMap.add setup.(_batFundDeposit) setup.(_batFund) FMap.empty;
+			EIP20Token.total_supply := setup.(_batFund);
+			EIP20Token.allowances := FMap.empty;
+		|} in
 	Some {|
 		(* EIP20Token initialisation: *)
-		total_supply := setup.(_batFund);
-		balances := FMap.add setup.(_batFundDeposit) setup.(_batFund) FMap.empty;
-		allowances := FMap.empty;
+		token_state := token_state;
 		(* BAT initialisation: *)
 		isFinalized := false;
 		fundDeposit := setup.(_fundDeposit);
@@ -114,6 +106,8 @@ Definition init (chain : Chain)
 	|}.
 
 Definition returnIf (cond : bool) := if cond then None else Some tt.
+Definition total_supply (state : State) := state.(token_state).(EIP20Token.total_supply).
+Definition balances (state : State) := state.(token_state).(EIP20Token.balances).
 
 Definition try_create_tokens sender (sender_payload : Amount) current_slot state :=
  (* early return if funding is finalized, funding period hasn't started yet, or funding period is over *)
@@ -123,26 +117,25 @@ Definition try_create_tokens sender (sender_payload : Amount) current_slot state
 	do _ <- returnIf (Z.leb sender_payload 0) ; 
 	(* Note: this conversion from Z to N is safe because by assumption sender_payload > 0 *)
 	let tokens := (Z.to_N sender_payload) * state.(tokenExchangeRate) in
-	let checkedSupply := state.(total_supply) + tokens in
+	let checkedSupply := (total_supply state) + tokens in
 	do _ <- returnIf (state.(tokenCreationCap) <? checkedSupply) ;
 	Some (state<|total_supply := checkedSupply|>
 						 <|balances    ::= FMap.add sender tokens|>).
 
 Definition try_refund sender current_slot state := 
 	(* early return if funding is finalized, or funding period is NOT over, or if total supply exceeds or is equal to the minimum fund tokens. *)
-	do _ <- returnIf (state.(isFinalized) || (Nat.leb current_slot state.(fundingEnd)) || (state.(tokenCreationMin) <=? state.(total_supply))) ;
+	do _ <- returnIf (state.(isFinalized) || (Nat.leb current_slot state.(fundingEnd)) || (state.(tokenCreationMin) <=? (total_supply state))) ;
 	(* Don't allow the the batFundDeposit account to refund *)
 	do _ <- returnIf (address_eqb sender state.(batFundDeposit)) ;
-	do sender_bats <- FMap.find sender state.(balances) ;
+	do sender_bats <- FMap.find sender (balances state) ;
 	do _ <- returnIf (sender_bats =? 0) ;
-	let new_total_supply := state.(total_supply) - sender_bats in
+	let new_total_supply := (total_supply) state - sender_bats in
 	(* convert tokens back to the currency of the blockchain, to be sent back to the sender address *)
 	let amount_to_send := Z.of_N (sender_bats / state.(tokenExchangeRate)) in
 	let new_state := state<|total_supply := new_total_supply|>
 												<|balances    ::= FMap.add sender 0|> in
 	let send_act := act_transfer sender amount_to_send in 
 		Some (new_state, [send_act]).
-	
 
 Definition try_finalize sender current_slot contract_balance state :=
 	(* early return if funding is finalized, or if sender is NOT the fund deposit address,
@@ -152,15 +145,15 @@ Definition try_finalize sender current_slot contract_balance state :=
 	*)
 	do _ <- returnIf (state.(isFinalized) || 
 									 (negb (address_eqb sender state.(fundDeposit))) || 
-									 (state.(total_supply) <? state.(tokenCreationMin))) ;
-	do _ <- returnIf ((Nat.leb current_slot state.(fundingEnd)) && negb (state.(total_supply) =? state.(tokenCreationCap))) ;
+									 ((total_supply state) <? state.(tokenCreationMin))) ;
+	do _ <- returnIf ((Nat.leb current_slot state.(fundingEnd)) && negb ((total_supply state) =? state.(tokenCreationCap))) ;
 	(* Send the currency of the contract back to the fund *)
 	let send_act := act_transfer state.(fundDeposit) contract_balance in
 	let new_state := state<|isFinalized := true|> in
 	Some (new_state, [send_act]).
 
 Open Scope Z_scope.
-Definition receive (chain : Chain)
+Definition receive_bat (chain : Chain)
 						 			 (ctx : ContractCallContext)
 									 (state : State)
 									 (maybe_msg : option Msg)
@@ -177,6 +170,20 @@ Definition receive (chain : Chain)
 	| _ => None
 	end.
 Close Scope Z_scope.
+
+(* Composes EIP20Token.receive and receive_bat by first executing EIP20Token.receive (if the message is an EIP20 message),
+	 and otherwise executes receive_bat *)
+Definition receive (chain : Chain)
+						 			 (ctx : ContractCallContext)
+									 (state : State)
+									 (maybe_msg : option Msg)
+									 : option (State * list ActionBody) :=
+	match maybe_msg with
+	| Some (tokenMsg msg) => do res <- EIP20Token.receive chain ctx state.(token_state) (Some msg) ;
+  									 let new_state := state<|token_state := fst res|> in 
+												 Some (new_state, snd res)
+	| _ => receive_bat chain ctx state maybe_msg
+	end.
 
 Ltac solve_contract_proper :=
   repeat
