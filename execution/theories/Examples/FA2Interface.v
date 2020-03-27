2d8c72697900d41711c5e3aaea7b126c1e2c4f7a
Mikkel Milo <mikkelmilo@gmail.com>
2020-03-27 15:32:30 +0100

    name changes, add setter and serializable instances

--- execution/theories/Examples/FA2Interface.v
+++ execution/theories/Examples/FA2Interface.v
@@ -1,5 +1,5 @@
 From Coq Require Import ZArith.
-From Coq Require Import List String.
+From Coq Require Import List.
 Require Import Serializable.
 Require Import Blockchain.
 
@@ -9,16 +9,18 @@ Require Import Containers.
 From RecordUpdate Require Import RecordUpdate.
 Import RecordSetNotations.
 
-Section FA2Types.
 Context {BaseTypes : ChainBase}.
-
+Set Primitive Projections.
+Set Nonrecursive Elimination Schemes.
+Section FA2Types.
 Definition token_id := N.
 
-Record transfer := {
-  from_ : Address;
-  to_ : Address;
-  transfer_token_id : token_id;
-  amount : N;
+Record transfer :=
+  build_transfer {
+    from_ : Address;
+    to_ : Address;
+    transfer_token_id : token_id;
+    amount : N;
 }.
 
 Record balance_of_request := {
@@ -48,10 +50,10 @@ Record total_supply_param := {
 
 Record token_metadata := {
   metadata_token_id : token_id;
-  metadata_symbol : string;
-  metadata_name : string;
+  (* metadata_symbol : string; *)
+  (* metadata_name : string; *)
   metadata_decimals : N;
-  metadata_extras : FMap string string;
+  (* metadata_extras : FMap string string; *)
 }.
 
 Record token_metadata_param := {
@@ -60,8 +62,8 @@ Record token_metadata_param := {
 }.
 
 Inductive operator_tokens :=
-  | All_tokens : operator_tokens
-  | Some_tokens : list token_id -> operator_tokens (* TODO: maybe set instead of list?*) .
+  | all_tokens : operator_tokens
+  | some_tokens : list token_id -> operator_tokens (* TODO: maybe set instead of list?*) .
 
 Record operator_param := {
   op_param_owner : Address;
@@ -70,9 +72,9 @@ Record operator_param := {
 }.
 
 Inductive update_operator :=
-  | Add_operator : operator_param -> update_operator
-  | Remove_operator : operator_param -> update_operator.
-
+  | add_operator : operator_param -> update_operator
+  | remove_operator : operator_param -> update_operator.
+  
 Record is_operator_response := {
   operator : operator_param;
   is_operator : bool;
@@ -86,20 +88,20 @@ Record is_operator_param := {
 (* permission policy definition *)
 
 Inductive self_transfer_policy :=
-  | Self_transfer_permitted : self_transfer_policy
-  | Self_transfer_denied : self_transfer_policy.
-
+  | self_transfer_permitted : self_transfer_policy
+  | self_transfer_denied : self_transfer_policy.
+  
 Inductive operator_transfer_policy :=
-  | Operator_transfer_permitted : operator_transfer_policy
-  | Operator_transfer_denied : operator_transfer_policy.
-
+  | operator_transfer_permitted : operator_transfer_policy
+  | operator_transfer_denied : operator_transfer_policy.
+  
 Inductive owner_transfer_policy :=
-  | Owner_no_op : owner_transfer_policy
-  | Optional_owner_hook : owner_transfer_policy
-  | Required_owner_hook : owner_transfer_policy.
-
+  | owner_no_op : owner_transfer_policy
+  | optional_owner_hook : owner_transfer_policy
+  | required_owner_hook : owner_transfer_policy.
+  
 Record custom_permission_policy := {
-  custom_policy_tag : string;
+  (* custom_policy_tag : string; *)
   custom_policy_config_api: option Address;
 }.
 
@@ -111,14 +113,14 @@ Record permissions_descriptor := {
   descr_custom : option custom_permission_policy;
 }.
 
-Inductive fa2_entry_points :=
+(* Inductive fa2_entry_points :=
   | Transfer : list transfer -> fa2_entry_points
   | Balance_of : balance_of_param -> fa2_entry_points
   | Total_supply : total_supply_param -> fa2_entry_points
   | Token_metadata : token_metadata_param -> fa2_entry_points
   | Permissions_descriptor : permissions_descriptor -> fa2_entry_points (* TODO fix callback type *)
   | Update_operators : list update_operator -> fa2_entry_points
-  | Is_operator : is_operator_param -> fa2_entry_points.
+  | Is_operator : is_operator_param -> fa2_entry_points. *)
 
 Record transfer_descriptor := {
   transfer_descr_from_ : option Address;
@@ -128,15 +130,135 @@ Record transfer_descriptor := {
 }.
 
 Record transfer_descriptor_param := {
-  tramsfer_descr_fa2 : Address;
-  tramsfer_descr_batch : list transfer_descriptor;
-  tramsfer_descr_operator : Address;
+  transfer_descr_fa2 : Address;
+  transfer_descr_batch : list transfer_descriptor;
+  transfer_descr_operator : Address;
 }.
 
 Inductive fa2_token_receiver :=
-  | Tokens_received : transfer_descriptor_param -> fa2_token_receiver.
+  | tokens_received : transfer_descriptor_param -> fa2_token_receiver.
 
 Inductive fa2_token_sender :=
-  | Tokens_sent : transfer_descriptor_param -> fa2_token_sender.
+  | tokens_sent : transfer_descriptor_param -> fa2_token_sender.
 
 End FA2Types.
+Section Setters.
+
+Instance transfer_settable : Settable transfer :=
+  settable! build_transfer <from_; to_; transfer_token_id; amount>.
+
+Instance balance_of_request_settable : Settable balance_of_request :=
+  settable! Build_balance_of_request <owner; bal_req_token_id>.
+
+Instance balance_of_response_settable : Settable balance_of_response :=
+  settable! Build_balance_of_response <request; balance>.
+
+Instance balance_of_param_settable : Settable balance_of_param :=
+  settable! Build_balance_of_param <bal_requests; bal_callback>.
+
+Instance total_supply_reponse_settable : Settable total_supply_response :=
+  settable! Build_total_supply_response <supply_resp_token_id; total_supply>.
+
+Instance total_supply_param_settable : Settable total_supply_param :=
+  settable! Build_total_supply_param <supply_param_token_ids; supply_param_callback>.
+
+Instance token_metadata_settable : Settable token_metadata :=
+  settable! Build_token_metadata <  metadata_token_id; metadata_decimals>.
+
+Instance token_metadata_param_settable : Settable token_metadata_param :=
+  settable! Build_token_metadata_param <  metadata_token_ids; metadata_callback>.
+
+Instance operator_param_settable : Settable operator_param :=
+  settable! Build_operator_param <op_param_owner; op_param_operator; op_param_tokens>.
+
+Instance is_operator_response_settable : Settable is_operator_response :=
+  settable! Build_is_operator_response <operator; is_operator>.
+
+Instance is_operator_param_settable : Settable is_operator_param :=
+  settable! Build_is_operator_param <is_operator_operator; is_operator_callback>.
+
+Instance custom_permission_policy_settable : Settable custom_permission_policy :=
+  settable! Build_custom_permission_policy <custom_policy_config_api>.
+
+Instance permissions_descriptor_settable : Settable permissions_descriptor :=
+  settable! Build_permissions_descriptor <descr_self; descr_operator; descr_receiver; descr_sender; descr_custom>.
+
+Instance transfer_descriptor_settable : Settable transfer_descriptor :=
+  settable! Build_transfer_descriptor <
+    transfer_descr_from_;
+    transfer_descr_to_;
+    transfer_descr_token_id;
+    transfer_descr_amount>.
+
+Instance transfer_descriptor_param_settable : Settable transfer_descriptor_param :=
+  settable! Build_transfer_descriptor_param <transfer_descr_fa2; transfer_descr_batch; transfer_descr_operator>.
+
+End Setters.
+
+Section Serialization.
+Global Instance transfer_serializable : Serializable transfer :=
+  Derive Serializable transfer_rect <build_transfer>.
+
+Global Instance balance_of_request_serializable : Serializable balance_of_request :=
+  Derive Serializable balance_of_request_rect <Build_balance_of_request>.
+
+Global Instance balance_of_response_serializable : Serializable balance_of_response :=
+  Derive Serializable balance_of_response_rect <Build_balance_of_response>.
+
+Global Instance balance_of_param_serializable : Serializable balance_of_param :=
+  Derive Serializable balance_of_param_rect <Build_balance_of_param>.
+
+Global Instance total_supply_response_serializable : Serializable total_supply_response :=
+  Derive Serializable total_supply_response_rect <Build_total_supply_response>.
+
+Global Instance total_supply_param_serializable : Serializable total_supply_param :=
+  Derive Serializable total_supply_param_rect <Build_total_supply_param>.
+
+Global Instance token_metadata_serializable : Serializable token_metadata :=
+  Derive Serializable token_metadata_rect <Build_token_metadata>.
+
+Global Instance token_metadata_param_serializable : Serializable token_metadata_param :=
+  Derive Serializable token_metadata_param_rect <Build_token_metadata_param>.
+
+Global Instance operator_tokens_serializable : Serializable operator_tokens :=
+  Derive Serializable operator_tokens_rect <all_tokens, some_tokens>.
+
+Global Instance operator_param_serializable : Serializable operator_param :=
+  Derive Serializable operator_param_rect <Build_operator_param>.
+
+Global Instance update_operator_serializable : Serializable update_operator :=
+  Derive Serializable update_operator_rect <add_operator, remove_operator>.
+
+Global Instance is_operator_response_serializable : Serializable is_operator_response :=
+  Derive Serializable is_operator_response_rect <Build_is_operator_response>.
+
+Global Instance is_operator_param_serializable : Serializable is_operator_param :=
+  Derive Serializable is_operator_param_rect <Build_is_operator_param>.
+
+Global Instance self_transfer_policy_serializable : Serializable self_transfer_policy :=
+  Derive Serializable self_transfer_policy_rect <self_transfer_permitted, self_transfer_denied>.
+
+Global Instance operator_transfer_policy_serializable : Serializable operator_transfer_policy :=
+  Derive Serializable operator_transfer_policy_rect <operator_transfer_permitted, operator_transfer_denied>.
+
+Global Instance owner_transfer_policy_serializable : Serializable owner_transfer_policy :=
+  Derive Serializable owner_transfer_policy_rect <owner_no_op , optional_owner_hook, required_owner_hook>.
+
+Global Instance custom_permission_policy_serializable : Serializable custom_permission_policy :=
+  Derive Serializable custom_permission_policy_rect <Build_custom_permission_policy>.
+
+Global Instance permissions_descriptor_serializable : Serializable permissions_descriptor :=
+  Derive Serializable permissions_descriptor_rect <Build_permissions_descriptor>.
+
+Global Instance transfer_descriptor_serializable : Serializable transfer_descriptor :=
+  Derive Serializable transfer_descriptor_rect <Build_transfer_descriptor>.
+
+Global Instance transfer_descriptor_param_serializable : Serializable transfer_descriptor_param :=
+  Derive Serializable transfer_descriptor_param_rect <Build_transfer_descriptor_param>.
+
+Global Instance fa2_token_receiver_serializable : Serializable fa2_token_receiver :=
+  Derive Serializable fa2_token_receiver_rect <tokens_received>.
+
+Global Instance fa2_token_sender_serializable : Serializable fa2_token_sender :=
+  Derive Serializable fa2_token_sender_rect <tokens_sent>.
+End Serialization.
