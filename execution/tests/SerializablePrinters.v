55a3ff3bfbba4c6e211585583bb4261428102e0b
Mikkel Milo <mikkelmilo@gmail.com>
2020-04-13 20:21:53 +0200

    cleanup imports

--- execution/tests/SerializablePrinters.v
+++ execution/tests/SerializablePrinters.v
@@ -1,22 +1,11 @@
 From ConCert Require Import Blockchain LocalBlockchain. 
-From ConCert Require BAT Congress Congress_Buggy EIP20Token FA2Token.
 From ConCert Require Import Serializable.
 From QuickChick Require Import QuickChick.
 From ConCert.Execution.QCTests Require Import CongressPrinters EIP20TokenPrinters FA2Printers TestContracts.
 
-(* Definition LocalChainBase : ChainBase := ChainGens.LocalChainBase. *)
-
 Local Open Scope string_scope.
 
 (* Currently we hack it to always deserialize to Msg types - only works for Congress! TODO: fix *)
-
-(* Instance showSerializable {ty : Type} `{Show ty} : Show (Serializable ty) :=
-{|
-  show s := match (@deserialize ty _ s) with
-            | Some ty => "SUCCESS"
-            | None => "FAIL"
-            end
-|}. *)
 
 Instance showSerializedValue : Show SerializedValue := 
 {|
@@ -41,6 +30,4 @@ Instance showSerializedValue : Show SerializedValue :=
     end
     end
 |}.
-
-
 Close Scope string_scope.
