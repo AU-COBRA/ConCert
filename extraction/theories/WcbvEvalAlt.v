52b7b04c51a266d383386d67cb6a534afbc08a27
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2020-06-30 00:57:57 +0200

    Finish proof of eval_deterministic
    This is false because of eval_fix_value, so we admit the (assumed)
    necessary change.

--- extraction/theories/WcbvEvalAlt.v
+++ extraction/theories/WcbvEvalAlt.v
@@ -1,26 +1,17 @@
 From ConCert.Extraction Require Import Aux.
-From ConCert.Extraction Require Import ClosedAux.
-From ConCert.Extraction Require Import ExAst.
-From ConCert.Extraction Require Import ExTyping.
-From ConCert.Extraction Require Import Optimize.
 From Coq Require Import Arith.
 From Coq Require Import Bool.
-From Coq Require Import String.
 From Coq Require Import List.
 From Coq Require Import Psatz.
 From Equations Require Import Equations.
+From MetaCoq.Erasure Require Import EAst.
 From MetaCoq.Erasure Require Import EAstUtils.
 From MetaCoq.Erasure Require Import ECSubst.
 From MetaCoq.Erasure Require Import EInduction.
-From MetaCoq.Erasure Require Import ELiftSubst.
 From MetaCoq.Erasure Require Import EWcbvEval.
 From MetaCoq.Template Require Import utils.
 
 Import ListNotations.
-
-Import EAstUtils.
-Import Erasure.
-Import ExAst.
 
 Reserved Notation "Σ ⊢ s ▷ t" (at level 50, s, t at next level).
 
@@ -65,7 +56,7 @@ Inductive aeval Σ : term -> term -> Prop :=
     Σ ⊢ f ▷ mkApps (tFix mfix idx) args ->
     Σ ⊢ a ▷ av ->
     cunfold_fix mfix idx = Some (narg, fn) ->
-    (#|args| < narg \/
+    (#|args| <> narg \/
      (#|args| = narg /\ ~is_ctor_or_box av)) ->
     Σ ⊢ tApp f a ▷ mkApps (tFix mfix idx) (args ++ [av])
 
@@ -192,7 +183,7 @@ Qed.
 
 Derive NoConfusionHom for EAst.global_decl.
 
-Lemma aeval_deterministic Σ t v v' :
+Lemma aeval_deterministic {Σ t v v'} :
   Σ ⊢ t ▷ v ->
   Σ ⊢ t ▷ v' ->
   v = v'.
@@ -370,6 +361,76 @@ Proof.
   - now depelim ev'.
 Qed.
 
+Lemma nth_error_snoc {A} (l : list A) (a : A) (n : nat) :
+  n = #|l| ->
+  nth_error (l ++ [a]) n = Some a.
+Proof.
+  intros ->.
+  rewrite nth_error_app2 by easy.
+  now rewrite Nat.sub_diag.
+Qed.
+
+Lemma aeval_stuck_fix_mkApps Σ f mfix idx args args' :
+  Σ ⊢ f ▷ tFix mfix idx ->
+  Forall2 (fun t v => Σ ⊢ t ▷ v) args args' ->
+  match nth_error mfix idx with
+  | Some d => (#|args| <= rarg d) \/ ~ETyping.is_constructor_or_box (rarg d) args'
+  | None => False
+  end ->
+  Σ ⊢ mkApps f args ▷ mkApps (tFix mfix idx) args'.
+Proof.
+  revert args'.
+  induction args as [|a args IH] using List.rev_ind;
+    intros args' ev ev_all stuck.
+  - apply Forall2_length in ev_all.
+    destruct args'; [|easy].
+    easy.
+  - destruct args' as [|a' args' _] using List.rev_ind;
+      [now apply Forall2_length in ev_all; rewrite app_length in *; cbn in *|].
+    apply Forall2_app_r in ev_all as (ev_all & ev_a).
+    rewrite mkApps_app.
+    cbn.
+    destruct (cunfold_fix mfix idx) as [(? & ?)|] eqn:cuf;
+      [|unfold cunfold_fix in *;
+        now destruct (nth_error _ _)].
+    eapply aeval_stuck_fix.
+    + apply IH; [easy|easy|].
+      destruct (nth_error _ _); [|easy].
+      rewrite app_length in stuck.
+      cbn in stuck.
+      destruct stuck; [left; lia|].
+      right.
+      unfold ETyping.is_constructor_or_box in *.
+      intros H'.
+      destruct (nth_error args' (rarg d)) eqn:nth; [|easy].
+      erewrite nth_error_app_left in H by easy.
+      congruence.
+    + easy.
+    + easy.
+    + unfold cunfold_fix in *.
+      destruct (nth_error _ _); [|easy].
+      rewrite app_length in *.
+      destruct stuck.
+      * cbn in *.
+        replace n with (rarg d) in * by congruence.
+        apply Forall2_length in ev_all.
+        left.
+        lia.
+      * unfold ETyping.is_constructor_or_box in H.
+        replace n with (rarg d) in * by congruence.
+        destruct (nth_error _ _) eqn:nth.
+        -- destruct (Nat.eqb_spec #|args'| (rarg d)); [|easy].
+           right.
+           split; [easy|].
+           rewrite nth_error_snoc in nth by easy.
+           noconf nth.
+           easy.
+        -- apply nth_error_None in nth.
+           rewrite app_length in *.
+           cbn in *.
+           left; lia.
+Qed.
+
 Lemma eval_aeval Σ t v :
   eval Σ t v ->
   Σ ⊢ t ▷ v.
@@ -383,20 +444,64 @@ Proof.
     cbn in *.
     apply Forall2_app_r in H2 as (H2 & ev_a).
     apply Forall2_app_r in H3 as (H3 & aev_a).
+    apply Forall2_length in H2 as len_eq.
+    rewrite app_length in *.
+    cbn in *.
     apply (aeval_fix Σ (mkApps f args) mfix idx args' narg fn a a' res); try easy.
-    + admit.
-    +
-    induction args.
-    + destruct args'; cbn in *.
-      2: { apply Forall2_length in H2.
+    + apply aeval_stuck_fix_mkApps; [easy|easy|].
+      unfold cunfold_fix in H.
+      destruct (nth_error mfix idx); [|easy].
+      noconf H.
+      left.
+      lia.
+    + unfold ETyping.is_constructor_or_box in H0.
+      now rewrite nth_error_snoc in H0 by lia.
+    + now rewrite mkApps_app.
+  - apply aeval_stuck_fix_mkApps; [easy|easy|].
+    unfold isStuckFix, ETyping.unfold_fix in H1.
+    destruct (nth_error mfix idx); [|easy].
+    assert (negb (ETyping.is_constructor_or_box (rarg d) args')) by admit.
+    propify.
+    right.
+    easy.
+Admitted.
+  (*- intros ev.
+    induction ev; try now econstructor.
+    + induction args using List.rev_ind.
+      * cbn in *.
+        change (tApp f a) with (mkApps f [a]).
+        change (tApp (tFix mfix idx) av) with (mkApps (tFix mfix idx) [av]).
+        apply eval_fix_value; [easy|easy|].
+        cbn.
+        unfold cunfold_fix, ETyping.unfold_fix in *.
+        destruct (nth_error _ _); [|easy].
+        noconf H.
+        destruct H0 as [?|(<- & ?)].
+        -- unfold ETyping.is_constructor.
+           rewrite (proj2 (nth_error_None _ _)); [easy|].
            cbn in *.
-           rewrite app_length in H2.
-           now cbn in *. }
-      replace narg with 0 in * by easy.
-      depelim H2.
-      depelim H4.
-      cbn in *.
-      now eapply (aeval_fix _ _ mfix idx []).
-    + cbn.
-  - now eapply aeval_box.
-  -
+           lia.
+        -- propify.
+           admit.
+      **)
+
+Lemma aeval_tApp_heads Σ hd hd' hdv arg v :
+  Σ ⊢ hd ▷ hdv ->
+  Σ ⊢ hd' ▷ hdv ->
+  Σ ⊢ tApp hd arg ▷ v ->
+  Σ ⊢ tApp hd' arg ▷ v.
+Proof.
+  intros ev_hd ev_hd' ev_app.
+  depind ev_app.
+  - rewrite (aeval_deterministic ev_hd ev_app1) in *.
+    now eapply aeval_box.
+  - rewrite (aeval_deterministic ev_hd ev_app1) in *.
+    now eapply aeval_beta.
+  - rewrite (aeval_deterministic ev_hd ev_app1) in *.
+    now eapply aeval_stuck_fix.
+  - rewrite (aeval_deterministic ev_hd ev_app1) in *.
+    now eapply aeval_fix.
+  - rewrite (aeval_deterministic ev_hd ev_app1) in *.
+    now eapply aeval_app_cong.
+  - easy.
+Qed.
