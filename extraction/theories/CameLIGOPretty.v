ab69755614b5ee4b6b58aeb25a5129d2eafe5740
Milo <mikkelmilo@users.noreply.github.com>
2020-11-17 10:13:50 +0100

    Update extraction/theories/CameLIGOPretty.v
    Co-authored-by: Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>

--- extraction/theories/CameLIGOPretty.v
+++ extraction/theories/CameLIGOPretty.v
@@ -123,12 +123,6 @@ Section print_term.
   | TAny => "UnknownType"
   end.
 
-  Compute print_box_type "" []
-          (TApp (TApp (TInd (mkInd <%% prod %%> 0)) (TVar 0)) (TVar 1)).
-
-  Compute print_box_type "" []
-          (TApp (TApp (TApp (TApp (TInd (mkInd (MPfile [], "list") 0)) (TVar 0)) (TVar 1)) (TVar 2))(TVar 3)).
-
 
   Definition print_ctor (prefix : string) (TT : env string) (ctor : ident × list box_type) :=
     let (nm,tys) := ctor in
