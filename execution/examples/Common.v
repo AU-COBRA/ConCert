81aa05bd70ee55b66ecbe17c7d956e03fc5f23e2
Danil <danil.v.annenkov@gmail.com>
2021-05-06 14:13:24 +0200

    Remove partial_alter (closes #91); fix inlining

---
+++ execution/examples/Common.v
@@ -0,0 +1,27 @@
+(** * Definitions shared among the examples *)
+
+Require Import Containers.
+From ConCert.Execution Require Import Blockchain.
+
+(** A type of  finite maps (dictionaries) with addresses as keys.
+Basically, it's just a specilisation of [FMap] to [Address] as keys.
+This definitions is more extraction-friendly. *)
+
+Module AddressMap.
+
+  Definition AddrMap `{ChainBase} := FMap Address.
+
+  Definition find `{ChainBase} {V : Type} (addr : Address) (m : AddrMap V) : option V :=
+    FMap.find addr m.
+
+  Definition add  `{ChainBase} {V : Type} (addr : Address) (val : V) (m : AddrMap V) : AddrMap V :=
+    FMap.add addr val m.
+
+  Definition empty  `{ChainBase} {V : Type} : AddrMap V := FMap.empty.
+
+End AddressMap.
+
+(** The specialised version is convertible to [FMap.find] after resolving the instances *)
+Lemma AddressMap_find_convertible  `{ChainBase} {V : Type} :
+  AddressMap.find (V:=V) = FMap.find.
+Proof. reflexivity. Qed.
