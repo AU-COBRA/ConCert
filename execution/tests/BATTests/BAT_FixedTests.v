3ca265f1a003865183d84ebbec2633fcd1b77436
Eske <eskehoy@gmail.com>
2021-05-11 22:44:53 +0200

    Balance chain generator in BAT_FixedTests

--- execution/tests/BATTests/BAT_FixedTests.v
+++ execution/tests/BATTests/BAT_FixedTests.v
@@ -28,7 +28,7 @@ Existing Instance BAT_FixedPrinters.showMsg.
 Definition ethFund : Address := BoundedN.of_Z_const AddrSize 16%Z.
 Definition batFund : Address := BoundedN.of_Z_const AddrSize 17%Z.
 
-Definition bat_setup := BAT_Fixed.build_setup (20%N) ethFund batFund 1 7 (3%N) (100%N) (60%N).
+Definition bat_setup := BAT_Fixed.build_setup (20%N) ethFund batFund 1 5 (3%N) (101%N) (90%N).
 Definition deploy_bat := create_deployment 0 BAT_Fixed.contract bat_setup.
 
 Let contract_base_addr := BoundedN.of_Z_const AddrSize 128%Z.
@@ -54,21 +54,21 @@ Module TestInfo <: BATGensInfo.
 End TestInfo.
 Module MG := BATGens TestInfo. Import MG.
 
+(* chain generator *)
 Definition gTokenChain max_acts_per_block token_cb max_length :=
   let act_depth := 1 in
   gChain_full_sized token_cb
     (fun env act_depth => gBATAction env) max_length act_depth max_acts_per_block.
-(* Sample (gTokenChain 2 token_cb 7). *) (* Sample generator to see quality of generated chains *)
 
-Definition chainDebug max_acts_per_block token_cb max_length :=
+(* Checker for debugging Action generator *)
+Definition chainDebug max_acts_per_block token_cb max_length g :=
   let act_depth := 1 in
   debug_gChain token_cb
-    (fun env act_depth => gBATActionValid env) max_length act_depth max_acts_per_block.
-(* QuickChick (chainDebug 1 token_cb 7). *) (* Debug generator to see if invalid call are generated *)
+    (fun env act_depth => g env) max_length act_depth max_acts_per_block.
 
-Definition forAllTokenChainTraces n :=
+Definition forAllTokenChainBuilders n :=
   let max_acts_per_block := 2 in
-  forAllChainState n token_cb (gTokenChain max_acts_per_block).
+  forAllChainBuilder n token_cb (gTokenChain max_acts_per_block).
 
 Definition pre_post_assertion_token P c Q :=
   let max_acts_per_block := 2 in
@@ -79,6 +79,120 @@ Notation "{{ P }} c {{ Q }}" := (pre_post_assertion_token P c Q) ( at level 50).
 Notation "cb '~~>' pf" := (reachableFrom_chaintrace cb (gTokenChain 2) pf) (at level 45, no associativity).
 Notation "'{' lc '~~~>' pf1 '===>' pf2 '}'" :=
   (reachableFrom_implies_chaintracePropSized 10 lc (gTokenChain 2) pf1 pf2) (at level 90, left associativity).
+
+
+
+(* Test generators *)
+
+(* Sample generator to see quality of generated chains *)
+(* Sample (gTokenChain 2 token_cb 7). *)
+
+(* Verify that gBATActionValid only produces valid actions *)
+(* QuickChick (chainDebug 1 token_cb 7 gBATActionValid). *)
+(* +++ Passed 10000 tests (0 discards) *)
+
+(* Get value of isFinalized in last state of a chain *)
+Definition get_chain_finalized (cb : ChainBuilder) : bool :=
+  match get_contract_state BAT_Fixed.State cb.(builder_env) contract_base_addr with
+  | Some state => state.(isFinalized)
+  | None => true
+  end.
+
+(* Verify hardness of finalizing BAToken.
+   Goal is ~ 2/3 of generated chains are finalized *)
+(* QuickChick (forAllTokenChainBuilders 8 (fun cb => collect (chain_is_finalized cb) true)). *)
+(*
+  6693 : true
+  3307 : false
+  +++ Passed 10000 tests (0 discards)
+*)
+
+(* Check if an action is finalize *)
+Definition action_is_finalize (action : Action) : bool :=
+  match action.(act_body) with
+  | act_transfer _ _ => false
+  | act_deploy _ _ _ => false
+  | act_call to _ msg =>
+    if (address_eqb to contract_base_addr)
+    then
+      match @deserialize BAT_Fixed.Msg _ msg with
+      | Some finalize => true
+      | Some _ => false
+      | None => false
+      end
+    else
+      false
+  end.
+
+(* Check if an action is refund *)
+Definition action_is_refund (action : Action) : bool :=
+  match action.(act_body) with
+  | act_transfer _ _ => false
+  | act_deploy _ _ _ => false
+  | act_call to _ msg =>
+    if (address_eqb to contract_base_addr)
+    then
+      match @deserialize BAT_Fixed.Msg _ msg with
+      | Some refund => true
+      | Some _ => false
+      | None => false
+      end
+    else
+      false
+  end.
+
+(* Get last state before finalize/refund in a chain *)
+Fixpoint get_last_funding_state {from to} (trace : ChainTrace from to) (default : ChainState) : ChainState :=
+  match trace with
+  | ChainedList.snoc trace' (Blockchain.step_action _ _ act _ _ _ _ _ as step) =>
+    if action_is_finalize act
+    then
+      fst (chainstep_states step)
+    else
+      if action_is_refund act
+      then
+        get_last_funding_state trace' (fst (chainstep_states step))
+      else
+        get_last_funding_state trace' default
+  | ChainedList.snoc trace' _ => get_last_funding_state trace' default
+  | ChainedList.clnil => default
+  end.
+
+(* Get the number of tokens in last state before finalize/refund in a chain *)
+Definition get_chain_tokens (cb : ChainBuilder) : TokenValue :=
+  let cs := get_last_funding_state cb.(builder_trace) empty_state in
+  match get_contract_state BAT_Fixed.State cs contract_base_addr with
+  | Some state => (total_supply state)
+  | None => 0%N
+  end.
+
+(* Verify spread of tokens after funding period is over.
+   We do this to see it it possible to hit the funding cap. *)
+(* QuickChick (forAllTokenChainBuilders 6 (fun cb => collect (get_chain_tokens cb) true)). *)
+(*
+  3290 : 101
+  1522 : 98
+  1194 : 95
+  968 : 92
+  855 : 89
+  643 : 86
+  490 : 83
+  354 : 80
+  247 : 77
+  175 : 74
+  111 : 71
+  79 : 68
+  32 : 65
+  15 : 62
+  12 : 59
+  6 : 0
+  5 : 56
+  1 : 53
+  1 : 47
+  +++ Passed 10000 tests (0 discards)
+*)
+
+
 
 Local Open Scope N_scope.
 
