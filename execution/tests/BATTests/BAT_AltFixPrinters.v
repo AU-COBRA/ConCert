9792c0de7b932530f96ddd943df3f96b2889e141
Eske <eskehoy@gmail.com>
2021-06-14 22:53:18 +0200

    Printers for BAT_AltFix

---
+++ execution/tests/BATTests/BAT_AltFixPrinters.v
@@ -0,0 +1,58 @@
+From ConCert Require Import Blockchain.
+From ConCert Require Import BAT_AltFix.
+From QuickChick Require Import QuickChick.
+
+From ConCert.Execution.QCTests Require Import
+  TestUtils EIP20TokenPrinters.
+
+Section BATPrinters.
+Context `{Show Address}.
+Local Open Scope string_scope.
+
+Existing Instance EIP20TokenPrinters.showMsg.
+Existing Instance EIP20TokenPrinters.showTokenState.
+
+Instance showTokenValue : Show TokenValue :=
+{|
+  show v := show v
+|}.
+
+Instance showMsg : Show BAT_AltFix.Msg :=
+{|
+  show m := match m with
+            | tokenMsg msg => show msg
+            | create_tokens => "create_tokens"
+            | finalize => "finalize"
+            | refund => "refund"
+            end
+|}.
+
+Instance showBATSetup : Show BAT_AltFix.Setup :=
+{|
+  show setup := "Setup{" ++
+    "initSupply: " ++ show setup.(_batFund) ++ sep ++
+    "fundDeposit: " ++ show setup.(_fundDeposit) ++ sep ++
+    "batFundDeposit: " ++ show setup.(_batFundDeposit) ++ sep ++
+    "fundingStart: " ++ show setup.(_fundingStart) ++ sep ++
+    "fundingEnd: " ++ show setup.(_fundingEnd) ++ sep ++
+    "tokenExchangeRate: " ++ show setup.(_tokenExchangeRate) ++ sep ++
+    "tokenCreationCap: " ++ show setup.(_tokenCreationCap) ++ sep ++
+    "tokenCreationMin: " ++ show setup.(_tokenCreationMin) ++ "}"
+|}.
+
+Instance showBATState : Show BAT_AltFix.State :=
+{|
+  show s := "State{" ++
+    "token_state: " ++ show s.(token_state) ++ sep ++
+    "isFinalized: " ++ show s.(isFinalized) ++ sep ++
+    "initSupply: " ++ show s.(initSupply) ++ sep ++
+    "fundDeposit: " ++ show s.(fundDeposit) ++ sep ++
+    "batFundDeposit: " ++ show s.(batFundDeposit) ++ sep ++
+    "fundingStart: " ++ show s.(fundingStart) ++ sep ++
+    "fundingEnd: " ++ show s.(fundingEnd) ++ sep ++
+    "tokenExchangeRate: " ++ show s.(tokenExchangeRate) ++ sep ++
+    "tokenCreationCap: " ++ show s.(tokenCreationCap) ++ sep ++
+    "tokenCreationMin: " ++ show s.(tokenCreationMin) ++ "}"
+|}.
+
+End BATPrinters.
