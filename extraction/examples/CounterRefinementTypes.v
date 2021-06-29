5711b79139c542b43cd7c917e2b02c4947847933
Mikkel Milo <mikkelmilo@gmail.com>
2021-06-29 15:52:03 +0200

    extract counterrefinement to cameligo

--- extraction/examples/CounterRefinementTypes.v
+++ extraction/examples/CounterRefinementTypes.v
@@ -40,7 +40,7 @@ Module CounterRefinementTypes.
   Definition storage := Z.
 
   Definition init (ctx : SimpleCallCtx) (setup : Z) : option storage :=
-    let ctx' := ctx in (* prevents optimisations from removing unused [ctx]  *)
+    let ctx_ := ctx in (* prevents optimisations from removing unused [ctx]  *)
     Some setup.
 
   Inductive msg := Inc (_ : Z) | Dec (_ : Z).
@@ -63,9 +63,9 @@ Module CounterRefinementTypes.
     Zleb_ltb_to_prop. lia.
   Qed.
 
-  Definition counter (Msg : msg) (st : storage)
+  Definition counter (msg : msg) (st : storage)
     : option (Transaction * storage) :=
-    match Msg with
+    match msg with
     | Inc i =>
       match (bool_dec true (0 <? i)) with
       | left H => Some (Transaction_none, proj1_sig (inc_counter st (exist _ i (eq_sym H))))
@@ -82,68 +82,73 @@ End CounterRefinementTypes.
 
 Import CounterRefinementTypes.
 
-(** [sig] and [exist] becomes just wrappers *)
-Definition sig_def := "type 'a sig_ = 'a".
-Definition exist_def := "let exist_ a = a".
+Section LiquidityExtractionSetup.
 
-(** A translation table for definitions we want to remap. The corresponding top-level definitions will be *ignored* *)
-Definition TT_remap : list (kername * string) :=
-  [ remap <%% bool %%> "bool"
-  ; remap <%% list %%> "list"
-  ; remap <%% Amount %%> "tez"
-  ; remap <%% address_coq %%> "address"
-  ; remap <%% time_coq %%> "timestamp"
-  ; remap <%% option %%> "option"
-  ; remap <%% Z.add %%> "addInt"
-  ; remap <%% Z.sub %%> "subInt"
-  ; remap <%% Z.leb %%> "leInt"
-  ; remap <%% Z.ltb %%> "ltInt"
-  ; remap <%% sig %%> "sig_" (* remapping [sig] to the wrapper *)
-  ; remap <%% @proj1_sig %%> "(fun x -> x)" (* this is a safe, but ad-hoc optimisation*)
-  ; remap <%% Z %%> "int"
-  ; remap <%% nat %%> "key_hash" (* type of account addresses*)
-  ; remap <%% Transaction %%> "operation list"
-  ; remap <%% @fst %%> "fst"
-  ; remap <%% @snd %%> "snd" ].
+  (** [sig] and [exist] becomes just wrappers *)
+  Definition sig_def := "type 'a sig_ = 'a".
+  Definition exist_def := "let exist_ a = a".
 
-(** A translation table of constructors and some constants. The corresponding definitions will be extracted and renamed. *)
-Definition TT_rename : list (string * string):=
-  [ ("Some", "Some")
-  ; ("None", "None")
-  ; ("Z0" ,"0")
-  ; ("nil", "[]")
-  ; ("true", "true")
-  ; ("exist", "exist_") (* remapping [exist] to the wrapper *)
-  ; (string_of_kername <%% storage %%>, "storage")  (* we add [storage] so it is printed without the prefix *) ].
+  (** A translation table for definitions we want to remap. The corresponding top-level definitions will be *ignored* *)
+  Definition TT_remap : list (kername * string) :=
+    [ remap <%% bool %%> "bool"
+    ; remap <%% list %%> "list"
+    ; remap <%% Amount %%> "tez"
+    ; remap <%% address_coq %%> "address"
+    ; remap <%% time_coq %%> "timestamp"
+    ; remap <%% option %%> "option"
+    ; remap <%% Z.add %%> "addInt"
+    ; remap <%% Z.sub %%> "subInt"
+    ; remap <%% Z.leb %%> "leInt"
+    ; remap <%% Z.ltb %%> "ltInt"
+    ; remap <%% sig %%> "sig_" (* remapping [sig] to the wrapper *)
+    ; remap <%% @proj1_sig %%> "(fun x -> x)" (* this is a safe, but ad-hoc optimisation*)
+    ; remap <%% Z %%> "int"
+    ; remap <%% nat %%> "key_hash" (* type of account addresses*)
+    ; remap <%% Transaction %%> "operation list"
+    ; remap <%% @fst %%> "fst"
+    ; remap <%% @snd %%> "snd" ].
 
-Definition COUNTER_MODULE : LiquidityMod msg _ Z storage ActionBody :=
-  {| (* a name for the definition with the extracted code *)
-     lmd_module_name := "liquidity_counter" ;
+  (** A translation table of constructors and some constants. The corresponding definitions will be extracted and renamed. *)
+  Definition TT_rename : list (string * string):=
+    [ ("Some", "Some")
+    ; ("None", "None")
+    ; ("Z0" ,"0")
+    ; ("nil", "[]")
+    ; ("true", "true")
+    ; ("exist", "exist_") (* remapping [exist] to the wrapper *)
+    ; (string_of_kername <%% storage %%>, "storage")  (* we add [storage] so it is printed without the prefix *) ].
 
-     (* definitions of operations on pairs and ints *)
-     lmd_prelude := concat nl [prod_ops;int_ops; sig_def; exist_def];
+  Definition COUNTER_MODULE : LiquidityMod msg _ Z storage ActionBody :=
+    {| (* a name for the definition with the extracted code *)
+      lmd_module_name := "liquidity_counter" ;
 
-     (* initial storage *)
-     lmd_init := init ;
+      (* definitions of operations on pairs and ints *)
+      lmd_prelude := concat nl [prod_ops;int_ops; sig_def; exist_def];
 
-     (* no extra operations in [init] are required *)
-     lmd_init_prelude := "" ;
+      (* initial storage *)
+      lmd_init := init ;
 
-     (* the main functionality *)
-     lmd_receive := counter ;
+      (* no extra operations in [init] are required *)
+      lmd_init_prelude := "" ;
 
-     (* code for the entry point *)
-     lmd_entry_point := printWrapper (PREFIX ++ "counter") ++ nl
-                       ++ printMain |}.
+      (* the main functionality *)
+      lmd_receive := counter ;
 
-(** We run the extraction procedure inside the [TemplateMonad].
-    It uses the certified erasure from [MetaCoq] and the certified deboxing procedure
-    that removes application of boxes to constants and constructors. *)
+      (* code for the entry point *)
+      lmd_entry_point := printWrapper (PREFIX ++ "counter") ++ nl
+                        ++ printMain |}.
 
-Definition to_inline := [<%% bool_rect %%>; <%% bool_rec %%>].
+  (** We run the extraction procedure inside the [TemplateMonad].
+      It uses the certified erasure from [MetaCoq] and the certified deboxing procedure
+      that removes application of boxes to constants and constructors. *)
+
+  Definition to_inline := [<%% bool_rect %%>; <%% bool_rec %%>].
+
+End LiquidityExtractionSetup.
+
 
 Time MetaCoq Run
-     (t <- liquidity_extraction PREFIX  TT_remap TT_rename to_inline COUNTER_MODULE ;;
+     (t <- liquidity_extraction PREFIX TT_remap TT_rename to_inline COUNTER_MODULE ;;
       tmDefinition COUNTER_MODULE.(lmd_module_name) t
      ).
 
@@ -151,3 +156,110 @@ Print liquidity_counter.
 
 (** We redirect the extraction result for later processing and compiling with the Liquidity compiler *)
 Redirect "examples/liquidity-extract/CounterRefinementTypes.liq" Compute liquidity_counter.
+
+
+
+From ConCert.Extraction Require Import CameLIGOPretty CameLIGOExtract.
+Module CameLIGOExtractionSetup.
+  
+
+  (** [sig] and [exist] becomes just wrappers *)
+  Definition sig_def_ligo := "type sig_ = int".
+  Definition exist_def_ligo := "let exist_ (a:int) = a".
+
+  (** A translation table for definitions we want to remap. The corresponding top-level definitions will be *ignored* *)
+  Definition TT_remap_ligo : list (kername * string) :=
+    [
+      remap <%% address_coq %%> "address"
+    ; remap <%% time_coq %%> "timestamp"
+    ; remap <%% Z.add %%> "addInt"
+    ; remap <%% Z.sub %%> "subInt"
+    ; remap <%% Z.leb %%> "leInt"
+    ; remap <%% Z.ltb %%> "ltInt"
+    ; remap <%% sig %%> "sig_" (* remapping [sig] to the wrapper *)
+    ; remap <%% @proj1_sig %%> "(fun (x:sig_) -> x)" (* this is a safe because of the way we define sig_, but ad-hoc optimisation*)
+    ; remap <%% Z %%> "int"
+    ; remap <%% Transaction %%> "operation list"
+    ].
+
+  (** A translation table of constructors and some constants. The corresponding definitions will be extracted and renamed. *)
+  Definition TT_rename_ligo : list (string * string):=
+    [ ("Some", "Some")
+    ; ("None", "None")
+    ; ("Z0" ,"0")
+    ; ("nil", "[]")
+    ; ("true", "true")
+    ; ("exist", "exist_") (* remapping [exist] to the wrapper *)
+    ; (string_of_kername <%% storage %%>, "storage")  (* we add [storage] so it is printed without the prefix *)
+    ; (string_of_kername <%% @ActionBody %%>, "operation") (* Same for ActionBody *) 
+    ; (string_of_kername <%% @Chain %%>, "chain") (* Same for Chain *) 
+    ].
+    
+
+  Definition counter_wrapper (c : Chain) 
+                             (ctx : SimpleCallCtx)
+                             (s : storage)
+                             (m : option msg) := 
+    let c_ := c in
+    let ctx_ := ctx in
+    match m with
+    | Some m => counter m s
+    | None => None
+    end.
+  
+Definition dummy_chain :=
+      "type chain = {
+        chain_height     : nat;
+        current_slot     : nat;
+        finalized_height : nat;
+        account_balance  : address -> nat
+      }"
+  ++ nl
+  ++ "let dummy_chain : chain = {
+        chain_height     = 0n;
+        current_slot     = 0n;
+        finalized_height = 0n;
+        account_balance  = fun (a : address) -> 0n
+      }".
+
+  Definition COUNTER_MODULE_LIGO : CameLIGOMod msg _ Z storage ActionBody :=
+    {| (* a name for the definition with the extracted code *)
+      lmd_module_name := "cameligo_counter" ;
+
+      (* definitions of operations on pairs and ints *)
+      lmd_prelude := concat nl [CameLIGOPrelude; sig_def_ligo; exist_def_ligo; dummy_chain];
+
+      (* initial storage *)
+      lmd_init := init ;
+
+      (* no extra operations in [init] are required *)
+      lmd_init_prelude := "" ;
+
+      (* the main functionality *)
+      lmd_receive := counter_wrapper ;
+
+      lmd_receive_prelude := "";
+      (* code for the entry point *)
+      lmd_entry_point := printWrapper (PREFIX ++ "counter_wrapper") 
+                                      (PREFIX ++"msg") 
+                                      "storage" 
+                                      CameLIGO_call_ctx 
+                                      ++ nl
+                                      ++ CameLIGOPretty.printMain |}.
+
+  (** We run the extraction procedure inside the [TemplateMonad].
+      It uses the certified erasure from [MetaCoq] and the certified deboxing procedure
+      that removes application of boxes to constants and constructors. *)
+
+  Definition to_inline_ligo := [<%% bool_rect %%>; <%% bool_rec %%>].
+
+  Time MetaCoq Run
+  (CameLIGO_prepare_extraction PREFIX to_inline_ligo TT_remap_ligo TT_rename_ligo COUNTER_MODULE_LIGO).
+
+  Time Definition cameLIGO_counter := Eval vm_compute in cameligo_counter_prepared.
+
+  MetaCoq Run (tmMsg cameLIGO_counter).
+  
+  Redirect "examples/liquidity-extract/CounterRefinementTypes.mligo" Compute cameLIGO_counter.
+    
+End CameLIGOExtractionSetup.
