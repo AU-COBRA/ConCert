3136c34ca2d5412aac9c2aac1459b9c199c15ce7
Eske <eskehoy@gmail.com>
2021-08-04 22:38:19 +0200

    Adapt existing proofs to new CallFacts signature

--- execution/examples/Congress.v
+++ execution/examples/Congress.v
@@ -302,7 +302,7 @@ Proof.
   - eauto.
   - eauto.
   - instantiate (AddBlockFacts := fun _ _ _ _ _ _ => True).
-    instantiate (CallFacts := fun _ _ _ => True).
+    instantiate (CallFacts := fun _ _ _ _ => True).
     instantiate (DeployFacts := fun _ _ => True).
     unset_all; subst.
     destruct step; auto.
@@ -493,7 +493,7 @@ Proof.
     now rewrite <- perm.
   - instantiate (AddBlockFacts := fun _ _ _ _ _ _ => True).
     instantiate (DeployFacts := fun _ _ => True).
-    instantiate (CallFacts := fun _ _ _ => True).
+    instantiate (CallFacts := fun _ _ _ _ => True).
     unset_all; subst.
     destruct step; auto.
     destruct a; auto.
