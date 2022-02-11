3873b7bfdb41be44c3f0fbf1bab95447555b5b49
4ever2 <3417013+4ever2@users.noreply.github.com>
2022-02-11 14:21:03 +0100

    Fix build on MacOS

--- extraction/process_extraction.sh
+++ extraction/process_extraction.sh
@@ -8,7 +8,7 @@ then
   for i in *.ml*
   do
     newi=`echo $i | cut -b 1 | tr '[:upper:]' '[:lower:]'``echo $i | cut -b 2-`;
-    if [[ ! $i -ef $newi ]]
+    if [ $i != $newi ]
     then
       echo "Moving " $i "to" $newi;
       mv $i $newi;
