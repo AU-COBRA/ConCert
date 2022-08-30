2d09f8b66669c7821d4cdde318fa8225b9bf0a3a
4ever2 <3417013+4ever2@users.noreply.github.com>
2022-08-30 13:56:42 +0200

    Fix Crowdfunding Liquidity wrapper and remapping

--- examples/crowdfunding/CrowdfundingLiquidity.v
+++ examples/crowdfunding/CrowdfundingLiquidity.v
@@ -36,6 +36,7 @@ Definition TT_remap : list (kername * String.string) :=
   ; remap <%% @fst %%> "fst"
   ; remap <%% @snd %%> "snd"
   ; remap <%% option %%> "option"
+  ; remap <%% ConCert.Execution.ResultMonad.result %%> "result"
   ; remap <%% Maps.addr_map_coq %%> "(address,tez) map"
   ; remap <%% SimpleActionBody_coq %%> "operation"
 
@@ -55,35 +56,41 @@ Definition TT_remap : list (kername * String.string) :=
 (** A translation table of constructors and some constants. The corresponding definitions will be extracted and renamed. *)
 Definition TT_rename :=
   [ ("Z0" ,"0DUN")
+  ; ("O", "0")
   ; ("nil", "[]")
   ; ("mnil", "Map []")
+  ; ("Ok", "Ok")
+  ; ("Err", "Err")
   ; ("tt", "()")
   ; ("true", "true")
   ; ("false", "false")].
 
 Definition printWrapperAndMain :=
   "let wrapper (msg : msg_coq)(st : ((timestamp * (tez * address)) * ((address,tez) map * bool))) = match receive msg st (Current.time (), (Current.sender (), (Current.amount (), Current.balance ()))) with
-| Ok v -> v| Err e -> failwith e" ++ Common.nl ++ Common.nl++
+| Some v -> v| None -> failwith 0" ++ Common.nl ++ Common.nl++
 "let%entry main (msg : msg_coq)(st : ((timestamp * (tez * address)) * ((address,tez) map * bool))) = wrapper msg st".
 
 
 Notation storage := ((time_coq × Z × address_coq) × Maps.addr_map_coq × bool).
 Notation params := ((time_coq × address_coq × Z × Z) × msg_coq).
+Definition Error : Type := nat.
+Definition default_error : Error := 0%nat.
+
 Definition crowdfunding_init (ctx : SimpleCallCtx)
                              (setup : (time_coq × Z × address_coq))
-                             : ConCert.Execution.ResultMonad.result storage nat :=
+                             : ConCert.Execution.ResultMonad.result storage Error :=
   if ctx.2.2.1 =? 0
   then ConCert.Execution.ResultMonad.Ok (setup, (Maps.mnil, false))
-  else ConCert.Execution.ResultMonad.Err 0%nat.
+  else ConCert.Execution.ResultMonad.Err default_error.
   (* (unfolded Init.init) setup ctx. *)
 
 Definition crowdfunding_receive
            (params : params)
            (st : storage)
-           : ConCert.Execution.ResultMonad.result (list SimpleActionBody_coq × storage) nat :=
+           : ConCert.Execution.ResultMonad.result (list SimpleActionBody_coq × storage) Error :=
   match receive params.2 st params.1 with
   | Some v => ConCert.Execution.ResultMonad.Ok v
-  | None => ConCert.Execution.ResultMonad.Err 0%nat
+  | None => ConCert.Execution.ResultMonad.Err default_error
   end.
 
 Definition CROWDFUNDING_MODULE :
@@ -106,14 +113,17 @@ Definition CROWDFUNDING_MODULE :
          ++ Common.nl
          ++ "let snd (p : 'a * 'b) : 'b = p.(1) in"
          ++ Common.nl
-         ++ "let eqTez (a : tez ) (b : tez ) = a = b in" ;
+         ++ "let eqTez (a : tez ) (b : tez ) = a = b in"
+         ++ Common.nl
+         ++ "let default_error = 0 in" ;
 
 
      (* the main functionality *)
      lmd_receive := crowdfunding_receive  ;
 
      (* code for the entry point *)
-     lmd_entry_point := printWrapperAndMain |}.
+     lmd_entry_point := printWrapper (PREFIX ++ "crowdfunding_receive") ++ Common.nl
+                        ++ printMain |}.
 
 (** We run the extraction procedure inside the [TemplateMonad].
     It uses the certified erasure from [MetaCoq] and the certified deboxing procedure
@@ -124,7 +134,6 @@ Time MetaCoq Run
       tmDefinition (String.of_string CROWDFUNDING_MODULE.(lmd_module_name)) t
      ).
 
-(* Print liquidity_crowdfunding. *)
-
 (** We redirect the extraction result for later processing and compiling with the Liquidity compiler *)
-Redirect "../extraction/tests/extracted-code/liquidity-extract/CrowdfundingCertifiedExtraction.liq" Compute liquidity_crowdfunding.
+Redirect "../extraction/tests/extracted-code/liquidity-extract/CrowdfundingCertifiedExtraction.liq"
+  Compute liquidity_crowdfunding.
