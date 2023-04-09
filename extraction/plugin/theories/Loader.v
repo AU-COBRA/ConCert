8afef8b2cbf7c72d3f31dd8daf5c3287468a9730
4ever2 <3417013+4ever2@users.noreply.github.com>
2023-04-09 02:04:47 +0200

    Remove old rust extraction

--- extraction/plugin/theories/Loader.v
+++
@@ -1,6 +0,0 @@
-From Coq Require Extraction.
-From MetaCoq.Template Require ExtractableLoader.
-
-(* Declare ML Module "extraction_plugin". *)
-(* Declare ML Module "coq-metacoq-template-coq.plugin". *)
-Declare ML Module "coq-concert-extraction.plugin".
