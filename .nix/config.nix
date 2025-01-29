955637c3b1dea97cd927df65b2bf7fdc247696f0
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-01-29 19:25:23 +0100

    Bump MetaCoq version

--- .nix/config.nix
+++ .nix/config.nix
@@ -10,7 +10,7 @@
 
   bundles."8.19" = {
     coqPackages.coq.override.version = "8.19";
-    coqPackages.metacoq.override.version = "coq-8.19";
+    coqPackages.metacoq.override.version = "1.3.3-8.19";
     coqPackages.stdpp.override.version = "1.10.0";
     coqPackages.QuickChick.override.version = "2.0.4";
     coqPackages.RustExtraction.override.version = "0.1.0";
