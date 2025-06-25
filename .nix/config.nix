3e71db3b6c6f31b0538cef06a36a9f2afb6bf4fd
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-06-26 00:15:28 +0200

    Support Coq 9.0

--- .nix/config.nix
+++ .nix/config.nix
@@ -16,6 +16,14 @@
     coqPackages.RustExtraction.override.version = "0.1.1";
     coqPackages.ElmExtraction.override.version = "0.1.1";
   };
+  bundles."9.0" = {
+    coqPackages.coq.override.version = "9.0";
+    coqPackages.metacoq.override.version = "1.3.4-9.0";
+    coqPackages.stdpp.override.version = "1.11.0";
+    coqPackages.QuickChick.override.version = "2.1.0";
+    coqPackages.RustExtraction.override.version = "0.1.1";
+    coqPackages.ElmExtraction.override.version = "0.1.1";
+  };
 
   cachix.coq = {};
   cachix.coq-community = {};
