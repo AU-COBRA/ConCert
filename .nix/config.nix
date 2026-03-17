f504fb3b9ea271b452f94a3a2f38d465af0c33aa
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-03-17 18:29:25 +0100

    Depend only on metarocq-erasure

--- .nix/config.nix
+++ .nix/config.nix
@@ -11,10 +11,10 @@
   bundles."9.1" = {
     coqPackages = {
       coq.override.version = "9.1";
-      metarocq.override.version = "1.4.1-9.1";
+      metarocq-erasure.override.version = "1.5.1-9.1";
       stdpp.override.version = "1.13.0";
       QuickChick.override.version = "2.1.1";
-      TypedExtraction.override.version = "0.2.0";
+      TypedExtraction.override.version = "0.2.1";
     };
     rocqPackages = {
       rocq-core.override.version = "9.1";
